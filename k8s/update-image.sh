14c1fae4fa666503abdd16b785bcb93cf7a64ba4
vishnuedla <edlavishnu@aol.com>
2026-07-07 16:36:55 +0530

    Add update-image.sh to modify deployment.yml[skip ci ]
    This script updates the image in the deployment.yml file by removing the old image line and adding a new one based on the provided username and GitHub run ID.

---
+++ k8s/update-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+username=vishnu4538 # docker account  username
+
+#delete image line
+
+sed -i {"/image:/d"} deployment.yml
+
+sleep 9s
+
+#Adding image line to file $1 is username of docker and $2 is for tag
+
+sed -i "19i\        image: docker.io/$username/${{ secrets.REPONAME }}:${{ github.run_id }}"  deployment.yml
+
+sleep 9s
